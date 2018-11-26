cef1b8323f678ad6b14c81374687690298992ba5
Sebastian <sebastiannot@hotmail.cl>
2018-11-26 11:53:15 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SalidasComponent } from './component/salidas/salidas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SalidasComponent
   ],
   imports: [
     BrowserModule,
